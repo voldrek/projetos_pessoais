bcffe720378af70e95f3f745d55bf548cbe70e2b
voldrek <lucasjordano91@gmail.com>
2022-09-20 20:47:53 -0300

    conexão com DB
    conexão com MySQL feita

--- voldrek20/login.py
+++ voldrek20/login.py
@@ -1,8 +1,10 @@
 from distutils.cmd import Command
 from hashlib import new
+from sqlite3 import Cursor
 from tkinter import *
 from tkinter import messagebox
 from random import randint
+import mysql.connector
 
 # cores-----------------
 co0 = '#f0f3f5' #preto
@@ -10,6 +12,11 @@ co1 = '#feffff' #branco
 co2 = '#3fb5a3' #verde
 co3 = '#38576b' #valor
 co4 = '#403d3d' #letra
+
+conexao = mysql.connector.connect(host='localhost',user='root',password='',database='voldrek20')
+
+Cursor = conexao.cursor
+
 
 def destruir():
     for widget in frame_baixo.winfo_children():
@@ -50,11 +57,7 @@ def verifica_senha():
     
         messagebox.showinfo('Login', 'Seja bem vindo de volta '+credenciais[0])
 
-        for widget in frame_baixo.winfo_children():
-            widget.destroy()
-        
-        for widget in frame_cima.winfo_children():
-            widget.destroy()
+        destruir()
         
         nova_janela()
         
@@ -63,12 +66,13 @@ def verifica_senha():
 
 def tela_cadastro():
 
-    for widget in frame_baixo.winfo_children():
-            widget.destroy()
-            
-    for widget in frame_cima.winfo_children():
-            widget.destroy()
+    destruir()
 
+    def cadastrar_bd_mysql():
+            #comando = 
+            Cursor.execute (f'''INSERT INTO login ('nome','senha','email') VALUES ('{e_c_nome}','{e_c_senha}','{e_c_email}')''')
+
+            
     #frame cima
     l_nome = Label(frame_cima, text='Cadastro' , anchor=NE , font=('Ivy 25') ,bg=co1 , fg=co4)
     l_nome.place(x=5 , y=5)
@@ -76,26 +80,28 @@ def tela_cadastro():
     l_linha.place(x=10 , y=45)
 
     #frame baixo
-    l_nome = Label(frame_baixo, text='Nome *' , anchor=NW , font=('Ivy 10') ,bg=co1 , fg=co4)
-    l_nome.place(x=10 , y=20)
-    e_nome = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
-    e_nome.place (x=14, y=50)
+    l_c_nome = Label(frame_baixo, text='Nome *' , anchor=NW , font=('Ivy 10') ,bg=co1 , fg=co4)
+    l_c_nome.place(x=10 , y=20)
+    e_c_nome = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
+    e_c_nome.place (x=14, y=50)
 
-    l_senha = Label(frame_baixo, text='senha *' , anchor=NW , font=('Ivy 10') ,bg=co1 , fg=co4)
-    l_senha.place(x=10 , y=80)
-    e_nome = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
-    e_nome.place (x=14, y=110)
+    l_c_senha = Label(frame_baixo, text='senha *' , anchor=NW , font=('Ivy 10') ,bg=co1 , fg=co4)
+    l_c_senha.place(x=10 , y=80)
+    e_c_senha = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
+    e_c_senha.place (x=14, y=110)
 
-    l_c_senha = Label(frame_baixo, text='confirmar  senha *' , anchor=NW , font=('Ivy 10') ,bg=co1 , fg=co4)
-    l_c_senha.place(x=10 , y=140)
-    e_nome = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
-    e_nome.place (x=14, y=160)
+    l_c_email = Label(frame_baixo, text='Email *' , anchor=NW , font=('Ivy 10') ,bg=co1 , fg=co4)
+    l_c_email.place(x=10 , y=140)
+    e_c_email = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief=SOLID)
+    e_c_email.place (x=14, y=160)
 
-    b_confirmar = Button(frame_baixo, command='', text='Confirmar' ,width=10,height=2, anchor=NW , font=('Ivy 8 bold') ,bg=co2 , fg=co1, relief=RAISED, overrelief=RIDGE)
+    b_confirmar = Button(frame_baixo, command=cadastrar_bd_mysql , text='Confirmar' ,width=10,height=2, anchor=NW , font=('Ivy 8 bold') ,bg=co2 , fg=co1, relief=RAISED, overrelief=RIDGE)
     b_confirmar.place(x=150 , y=210)
 
+    
 
 def nova_janela():
+
     def D20():
         d20 = randint(1 , 20)
         resultado['text'] = d20
@@ -114,6 +120,7 @@ def nova_janela():
     def D4():
         d4 = randint(1 , 4)
         resultado['text'] = d4
+
     l_nome = Label(frame_cima, text='Usuário: '+credenciais[0] , anchor=NE , font=('Ivy 20') ,bg=co1 , fg=co4)
     l_nome.place(x=5 , y=5)
 
@@ -160,4 +167,7 @@ b_confirmar.place(x=15 , y=180)
 
 b_cadastro = Button(frame_baixo, command=tela_cadastro, text='Cadastro' ,width=10,height=2, anchor=NW , font=('Ivy 8 bold') ,bg=co2 , fg=co1, relief=RAISED, overrelief=RIDGE)
 b_cadastro.place(x=200 , y=180)
+
 login.mainloop()
+Cursor.close()
+conexao.close()
